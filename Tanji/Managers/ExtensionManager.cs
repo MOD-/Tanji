1d906be496ccd25171cf9f15f7a01f3c1a255d3c
ArachisH <TheOldNutMan@gmail.com>
2015-08-03 15:23:48 -0700

    Toggle UI items based on whether the user has an extension selected.

--- Tanji/Managers/ExtensionManager.cs
+++ Tanji/Managers/ExtensionManager.cs
@@ -43,8 +43,18 @@ namespace Tanji.Managers
             MainUI = main;
             ExtensionAction += ExtensionManager_ExtensionAction;
 
+            MainUI.ETOpenMenuBtn.Click += ETOpenMenuBtn_Click;
+            MainUI.ETInstallMenuBtn.Click += ETInstallExtensionBtn_Click;
+            MainUI.ETUninstallMenuBtn.Click += ETUninstallExtensionBtn_Click;
+
             MainUI.ETInstallExtensionBtn.Click += ETInstallExtensionBtn_Click;
+            MainUI.ETUninstallExtensionBtn.Click += ETUninstallExtensionBtn_Click;
+            MainUI.ETExtensionAOTChckbx.CheckedChanged += ETExtensionAOTChckbx_CheckedChanged;
+
             MainUI.ContractorVw.InitializeContractor(this);
+
+            MainUI.ContractorVw.ItemSelected += ContractorVw_ItemSelected;
+            MainUI.ContractorVw.ItemsDeselected += ContractorVw_ItemsDeselected;
 
             _openFileDialog = new OpenFileDialog();
             _openFileDialog.DefaultExt = "dll";
@@ -55,6 +65,29 @@ namespace Tanji.Managers
             MainUI.Connection.DataOutgoing += DataOutgoing;
         }
 
+        private void ETOpenMenuBtn_Click(object sender, EventArgs e)
+        {
+            MainUI.ContractorVw.OpenSelected();
+        }
+        private void ETInstallExtensionBtn_Click(object sender, EventArgs e)
+        {
+            _openFileDialog.FileName = _openFileDialog.SafeFileName;
+            if (_openFileDialog.ShowDialog() != DialogResult.OK) return;
+
+            MainUI.ContractorVw.Install(_openFileDialog.FileName);
+        }
+        private void ETUninstallExtensionBtn_Click(object sender, EventArgs e)
+        {
+            MainUI.ContractorVw.UninstallSelected();
+        }
+        private void ETExtensionAOTChckbx_CheckedChanged(object sender, EventArgs e)
+        {
+            ExtensionForm extension = MainUI.ContractorVw.GetSelected();
+            if (extension == null) return;
+
+            extension.TopMost = MainUI.ETExtensionAOTChckbx.Checked;
+        }
+
         private void ExtensionManager_ExtensionAction(object sender, ExtensionActionEventArgs e)
         {
             int runningCount = Extensions.Count(ext => ext.IsRunning);
@@ -63,12 +96,24 @@ namespace Tanji.Managers
                 $"Extensions Active: {runningCount}/{Extensions.Count}";
         }
 
-        private void ETInstallExtensionBtn_Click(object sender, EventArgs e)
+        private void ContractorVw_ItemsDeselected(object sender, EventArgs e)
         {
-            _openFileDialog.FileName = _openFileDialog.SafeFileName;
-            if (_openFileDialog.ShowDialog() != DialogResult.OK) return;
+            MainUI.ETExtensionAOTChckbx.Enabled = false;
+            MainUI.ETUninstallExtensionBtn.Enabled = false;
 
-            MainUI.ContractorVw.Install(_openFileDialog.FileName);
+            MainUI.ETOpenMenuBtn.Enabled = false;
+            MainUI.ETUninstallMenuBtn.Enabled = false;
+        }
+        private void ContractorVw_ItemSelected(object sender, ListViewItemSelectionChangedEventArgs e)
+        {
+            if (!MainUI.ETUninstallExtensionBtn.Enabled)
+            {
+                MainUI.ETUninstallExtensionBtn.Enabled =
+                    MainUI.ETExtensionAOTChckbx.Enabled =
+                    MainUI.ETOpenMenuBtn.Enabled =
+                    MainUI.ETUninstallMenuBtn.Enabled =
+                    MainUI.ETInstallMenuBtn.Enabled = true;
+            }
         }
 
         private void DataIncoming(object sender, InterceptedEventArgs e) =>
