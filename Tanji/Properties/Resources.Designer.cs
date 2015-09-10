7db85be1335ce316a77f1bf3ec8d18c6c65d00fc
ArachisH <TheOldNutMan@gmail.com>
2015-09-10 15:08:40 -0700

    Removed TanjiModePanel. Version: 1.3.0.0

--- Tanji/Properties/Resources.Designer.cs
+++ Tanji/Properties/Resources.Designer.cs
@@ -69,5 +69,15 @@ namespace Tanji.Properties {
                 return ((System.Drawing.Icon)(obj));
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap Tanji64 {
+            get {
+                object obj = ResourceManager.GetObject("Tanji64", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
