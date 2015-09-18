e7cb744735b0fc6ab9b908ff137afa618a6dbfb6
ArachisH <TheOldNutMan@gmail.com>
2015-09-17 19:03:17 -0700

    Revert Tanji source code back to 1.2.6 due to Sulakore compatibilitiy issues.

---
+++ Tanji/Managers/UpdateManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tanji.Managers
+{
+    public class UpdateManager
+    {
+        public MainFrm MainUI { get; }
+
+        public UpdateManager(MainFrm main)
+        {
+            MainUI = main;
+        }
+    }
+}
