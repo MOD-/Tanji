d6881d54e8b56e8044c56836d6d5c11ff2c663cc
ArachisH <theoldnutman@gmail.com>
2016-02-05 19:03:42 -0800

    These things handle data and stuff, well, at least that's what it should mean when being implemented, duh.

---
+++ Tanji/Pages/IDataHandler.cs
@@ -0,0 +1,13 @@
+﻿using Sulakore.Communication;
+
+namespace Tanji.Pages
+{
+    public interface IDataHandler
+    {
+        bool IsHandlingOutgoing { get; }
+        bool IsHandlingIncoming { get; }
+
+        void HandleOutgoing(InterceptedEventArgs e);
+        void HandleIncoming(InterceptedEventArgs e);
+    }
+}
---
+++ Tanji/Pages/IDataManager.cs
@@ -0,0 +1,12 @@
+﻿using Sulakore.Communication;
+
+namespace Tanji.Pages
+{
+    public interface IDataManager
+    {
+        HConnection Connection { get; }
+
+        void AddDataHandler(IDataHandler dataHandler);
+        void RemoveDataHandler(IDataHandler dataHandler);
+    }
+}
