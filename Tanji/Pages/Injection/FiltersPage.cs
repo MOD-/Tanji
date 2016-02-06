8d96a00dcb48ace7081d42740f1290296c6e1fd7
ArachisH <theoldnutman@gmail.com>
2016-02-05 23:10:19 -0800

    Implements IDataHandler, logic will come later, but this object will certainly need block/replace capabilities.

--- Tanji/Pages/Injection/FiltersPage.cs
+++ Tanji/Pages/Injection/FiltersPage.cs
@@ -1,13 +1,23 @@
 ﻿using System.Windows.Forms;
 
+using Sulakore.Communication;
+
 namespace Tanji.Pages.Injection
 {
-    public class FiltersPage : TanjiSubPage<InjectionPage>
+    public class FiltersPage : TanjiSubPage<InjectionPage>, IDataHandler
     {
+        public bool IsHandlingOutgoing { get; private set; } = false;
+        public bool IsHandlingIncoming { get; private set; } = false;
+
         public FiltersPage(InjectionPage parent, TabPage tab)
             : base(parent, tab)
         {
-            UI.InjectionTabs.TabPages.Remove(UI.FiltersTab);
+            UI.InjectionTabs.TabPages.Remove(tab);
         }
+
+        public void HandleOutgoing(InterceptedEventArgs e)
+        { }
+        public void HandleIncoming(InterceptedEventArgs e)
+        { }
     }
 }
