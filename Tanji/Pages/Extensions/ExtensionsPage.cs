be59a9249f34f3bdff55a95c38c4527447d0428b
ArachisH <theoldnutman@gmail.com>
2016-02-05 23:14:17 -0800

    Implements IDataHandler, essential for distributing all incoming/outgoing data to installed extensions.

--- Tanji/Pages/Extensions/ExtensionsPage.cs
+++ Tanji/Pages/Extensions/ExtensionsPage.cs
@@ -1,11 +1,21 @@
 ﻿using System.Windows.Forms;
 
+using Sulakore.Communication;
+
 namespace Tanji.Pages.Extensions
 {
-    public class ExtensionsPage : TanjiPage
+    public class ExtensionsPage : TanjiPage, IDataHandler
     {
         public ExtensionsPage(MainFrm ui, TabPage tab)
             : base(ui, tab)
+        { }
+
+        public bool IsHandlingOutgoing { get; } = false;
+        public bool IsHandlingIncoming { get; } = false;
+
+        public void HandleOutgoing(InterceptedEventArgs e)
+        { }
+        public void HandleIncoming(InterceptedEventArgs e)
         { }
     }
 }
