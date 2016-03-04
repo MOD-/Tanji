a36cd39439e50db7a6b04b4f63e60b02a05a301f
ArachisH <theoldnutman@gmail.com>
2016-03-04 14:32:32 -0800

    Makes it easier for forms to bind stuff.

--- Tanji/Components/TanjiForm.cs
+++ Tanji/Components/TanjiForm.cs
@@ -1,14 +1,41 @@
-﻿using Tanji.Properties;
-
+﻿using System;
 using System.Drawing;
 using System.Windows.Forms;
+using System.ComponentModel;
+
+using Tanji.Properties;
 
 namespace Tanji.Components
 {
-    public class TanjiForm : Form
+    public class TanjiForm : Form, INotifyPropertyChanged
     {
+        private readonly Action<PropertyChangedEventArgs> _onPropertyChanged;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void RaiseOnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                OnPropertyChanged(
+                    new PropertyChangedEventArgs(propertyName));
+            }
+        }
+        protected virtual void OnPropertyChanged(PropertyChangedEventArgs e)
+        {
+            if (InvokeRequired)
+            {
+                Invoke(_onPropertyChanged, e);
+            }
+            else
+            {
+                PropertyChanged?.Invoke(this, e);
+            }
+        }
+
         public TanjiForm()
         {
+            _onPropertyChanged = OnPropertyChanged;
+
             BackColor = Color.White;
             Icon = Resources.Tanji_128;
         }
