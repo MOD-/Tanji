a07b4b381e7cae1001bf75f64fa11d4085c2aa23
ArachisH <theoldnutman@gmail.com>
2016-03-19 22:04:51 -0700

    We no longer need to run as administrator, nice nice.

--- Tanji/Program.cs
+++ Tanji/Program.cs
@@ -1,11 +1,7 @@
 ﻿using System;
-using System.Diagnostics;
 using System.Windows.Forms;
-using System.Security.Principal;
 
 using Eavesdrop;
-
-using Sulakore.Communication;
 
 namespace Tanji
 {
@@ -14,44 +10,21 @@ namespace Tanji
         [STAThread]
         private static void Main()
         {
-            var windowsPrincipal = new WindowsPrincipal(WindowsIdentity.GetCurrent());
-            if (!windowsPrincipal.IsInRole(WindowsBuiltInRole.Administrator))
-            {
-                using (var tanjiProcess = new Process())
-                {
-                    tanjiProcess.StartInfo.Verb = "runas";
-                    tanjiProcess.StartInfo.FileName = "Tanji.exe";
-                    tanjiProcess.StartInfo.UseShellExecute = true;
-                    tanjiProcess.Start();
-                }
-            }
-            else
-            {
-                AppDomain.CurrentDomain.UnhandledException +=
-                    UnhandledException;
+            AppDomain.CurrentDomain
+                .UnhandledException += UnhandledException;
 
-                Eavesdropper.Terminate();
-                HConnection.RestoreHosts();
-
-                Application.EnableVisualStyles();
-                Application.SetCompatibleTextRenderingDefault(false);
-                Application.Run(new MainFrm());
-
-                Eavesdropper.Terminate();
-                HConnection.RestoreHosts();
-            }
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new MainFrm());
         }
         private static void UnhandledException(object sender, UnhandledExceptionEventArgs e)
         {
             var exception = (Exception)e.ExceptionObject;
             MessageBox.Show($"Message: {exception.Message}\r\n\r\n{exception.StackTrace.Trim()}",
-                "Tanji ~ Critical Error!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                "Tanji ~ Error!", MessageBoxButtons.OK, MessageBoxIcon.Error);
 
             if (e.IsTerminating)
-            {
                 Eavesdropper.Terminate();
-                HConnection.RestoreHosts();
-            }
         }
     }
 }
