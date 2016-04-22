1574020d13779c4ea9b9d2f45aaeb294575626df
ArachisH <TheOldNutMan@gmail.com>
2016-04-22 00:57:41 -0700

    Implemented 'IRetrievable' interface. Added 'Save As' button to save the packet in raw form(byte[]).

--- Tanji/Pages/Injection/PrimitivePage.cs
+++ Tanji/Pages/Injection/PrimitivePage.cs
@@ -1,29 +1,48 @@
-﻿using System.Windows.Forms;
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+using Tanji.Manipulators;
 
 using Sulakore.Protocol;
 
 namespace Tanji.Pages.Injection
 {
-    public class PrimitivePage : TanjiSubPage<InjectionPage>
+    public class PrimitivePage : TanjiSubPage<InjectionPage>, IRetrievable
     {
         public PrimitivePage(InjectionPage parent, TabPage tab)
             : base(parent, tab)
-        { }
-
-        private void PTPacketTxt_TextChanged(object sender, System.EventArgs e)
         {
-            var packet = new HMessage(UI.PTPacketTxt.Text);
+            UI.PTSaveAsBtn.Click += PTSaveAsBtn_Click;
+        }
+
+        private void PTSaveAsBtn_Click(object sender, EventArgs e)
+        {
+            UI.SavePacketDlg.FileName = string.Empty;
+            if (UI.SavePacketDlg.ShowDialog() != DialogResult.OK) return;
+
+            File.WriteAllBytes(UI.SavePacketDlg.FileName,
+                GetPacket().ToBytes());
+        }
+        private void PTPacketTxt_TextChanged(object sender, EventArgs e)
+        {
+            HMessage packet = GetPacket();
             UI.ITPacketTxt.Text = packet.ToString();
 
-            UI.PTPacketInfoLbl.Text =
-                $"Header: {packet.Header}, Length: {packet.Length}";
+            UI.PTHeaderTxt.Value = packet.Header.ToString();
+            UI.PTLengthTxt.Value = packet.Length.ToString("n0");
 
-            UI.PTCorruptedValueLbl.Text =
+            UI.PTCorruptedTxt.Value =
                 packet.IsCorrupted.ToString();
 
-            UI.PTCorruptedValueLbl.ForeColor = (packet.IsCorrupted ?
+            UI.PTCorruptedTxt.BackColor = (packet.IsCorrupted ?
                 UI.PacketLoggerUI.IncomingHighlight :
                 UI.PacketLoggerUI.OutgoingHighlight);
+        }
+
+        public HMessage GetPacket()
+        {
+            return new HMessage(UI.PTPacketTxt.Text);
         }
 
         protected override void OnTabSelecting(TabControlCancelEventArgs e)
